c79151833a5760d44a47ebab9d65c6aaad12d9b9
paliankit <paliankit999@gmail.com>
2025-09-06 22:24:21 +0530

    first commit

---
+++ src/GamePanel.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel {
+
+    public GamePanel(){
+        this.setPreferredSize(new Dimension(600,600));
+        this.setBackground(Color.black);
+        this.setFocusable(true);
+
+
+    }
+
+}
